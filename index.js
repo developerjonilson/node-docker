a248fed31b6884b01295016fe426b44a5b04961b
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-04-24 12:01:09 -0300

    docker live concluded

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect(`mongodb://${process.env.DB_URL}:27017/nodemongo`, {
+  useNewUrlParser: true
+});
+
 app.get("/", (req, res) => {
-  return res.send("Hello World");
+  return res.send("Hello World 3");
 });
 
 app.listen(3000);
